74e3993d7beb9308ae2c466c6c946d51d755ae63
SacariLuz <asacaricachi@gmail.com>
2024-01-14 17:12:54 -0800

    correction setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,8 @@
-# MySQL commands
+-- Prepares a MyQSL for the proyects
+-- CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-# havin doubts bout this one
-# GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
+FLUSH PRIVILEGES;
