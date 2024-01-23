5eca1446b781cf53e2e6056230c4bbd6c6f73203
SacariLuz <asacaricachi@gmail.com>
2024-01-23 09:14:07 -0800

    correction de pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,12 +7,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_word():
     """
     Function to print
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
