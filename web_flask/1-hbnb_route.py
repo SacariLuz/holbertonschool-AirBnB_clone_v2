ffedafb8bc280fc386c42bc994188c253292ade7
SacariLuz <asacaricachi@gmail.com>
2024-01-24 09:02:58 -0800

    correction pycodestyle 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,9 +11,10 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
-    Display "Hello HBNB!" 
+    Display "Hello HBNB!"
     """
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
