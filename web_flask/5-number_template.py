60883b74a51bb5e0bf7403730144e247d21d2e79
SacariLuz <asacaricachi@gmail.com>
2024-01-28 17:55:11 -0800

    add display a html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -56,7 +56,7 @@ def number_template(n):
     """
     Display a HTML page only if n is an integer
     """
-    return render_template("5-number.html", n=n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
