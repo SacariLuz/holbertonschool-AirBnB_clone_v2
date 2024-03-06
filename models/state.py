369e3431d88889e82e97e618ebfeae3e1ac7f054
SacariLuz <asacaricachi@gmail.com>
2024-03-06 17:16:37 +0000

    correction files

--- models/state.py
+++ models/state.py
@@ -10,23 +10,25 @@ from sqlalchemy.orm import relationship
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     class State(BaseModel, Base):
-        """ State class for ORM"""
-        __tablename__ = 'states'
+        """ State class """
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
+
+        # For DBStorage
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+
 else:
     class State(BaseModel):
-        """ State class """
+        """State class for FS"""
         name = ''
 
+        # For FileStorage
         @property
         def cities(self):
             from models import storage
-            list_cities = []
-
+            cities_list = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    list_cities.append(city)
-
-            return list_cities
+                    cities_list.append(city)
+            return cities_list
